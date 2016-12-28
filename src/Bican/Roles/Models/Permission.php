c1ac8d0b16109dda3de2d8570d6aa281b0228a03
Hieupv <hieupv@Hieupvs-MacBook-Pro.local>
2016-12-28 11:12:52 +0700

    5.3 compatible

---
+++ src/Bican/Roles/Models/Permission.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NF\Roles\Models;
+
+use NF\Roles\Traits\Slugable;
+use Illuminate\Database\Eloquent\Model;
+use NF\Roles\Traits\PermissionHasRelations;
+use NF\Roles\Contracts\PermissionHasRelations as PermissionHasRelationsContract;
+
+class Permission extends Model implements PermissionHasRelationsContract
+{
+    use Slugable, PermissionHasRelations;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'slug', 'description', 'model'];
+
+    /**
+     * Create a new model instance.
+     *
+     * @param array $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if ($connection = config('roles.connection')) {
+            $this->connection = $connection;
+        }
+    }
+}
